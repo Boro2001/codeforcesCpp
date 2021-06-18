31dba766e7d614b607c6d28ab2da9fd83613863d
Boro2001 <mikolajborowicz7@gmail.com>
2021-06-18 13:23:19 +0200

    siemano

--- src/23ZawansowaneProgramowanieObiektowe.cpp
+++ src/23ZawansowaneProgramowanieObiektowe.cpp
@@ -27,6 +27,35 @@ public:
       const zespolona & operator*(const zespolona &);
       bool operator==(const zespolona &);
 };
+zespolona::zespolona(double x, double y){
+      re = x; im = y; 
+}
+zespolona::zespolona(const zespolona & other){
+      re = other.re;
+      im = other.im;
+}
+zespolona::zespolona(zespolona && other){
+      re = other.re;
+      im = other.im;
+}
+const zespolona & zespolona::operator+(const zespolona & other){
+      re += other.re;
+      im += other.im;
+      return * this;
+}
+const zespolona & zespolona::operator-(const zespolona & other){
+      re -= other.re;
+      im -= other.im;
+      return * this;
+}
+const zespolona & zespolona::operator*(const zespolona & other){
+      //(a + ib)(c + id)= ac - bd + i(bc+ad)
+      zespolona * tmp =  new zespolona(re*other.re - im*other.im, im*other.re+re*other.im);
+      return * tmp;
+}
+namespace zesp{
+      const zespolona i(0,1);
+}
 
 int main(){
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,186 +1,36 @@
 #include <iostream>
+#include <list>
+#include <map>
+#include <string>
+
 using namespace std;
-
-class Vector{
-protected:
-      double * start; 
-      double * end; 
-      int capacity;
-      bool reserve(int newCapacity);
+class point{
 public:
-      Vector(int i = 0);
-      Vector(const Vector &);
-      Vector(Vector &&);
-      ~Vector();
-
-      int getSize() const;
-      bool pushFront(double);
-      bool pushBack(double);
-      bool insertAt(int, double);
-      bool deleteFront();
-      bool deleteBack();
-      double &elementAt(int)const; // dostęp do itego el
-      double & operator[](int);
-      void dump() const;
-};
-
-Vector::Vector(int size){
-      start = new double[size];
-      end = start;
-      capacity = size;
-}
-Vector::~Vector(){
-      if (start!=nullptr) delete [] start;
-      start = nullptr; 
-      end = nullptr;
-      capacity = 0;
-}
-int Vector::getSize()const{
-      return end-start;
-}
-bool Vector::reserve(int newCapacity){
-      if(newCapacity < capacity) return false; 
-      double * tmp = new double[newCapacity];
-      if(capacity){//jeśli jest większe niż zero, kopiowanie el. tablicy
-            for(int i =0; i<capacity; ++i)tmp[i]=start[i];
-            delete [] start;
-      }
-      end = tmp + (end - start);
-      start = tmp;
-      return true;
-}
-bool Vector::pushBack(double v) {
-    if(capacity==getSize() && !reserve(capacity+64)) return false;
-    start[getSize()]=v; // czyli *end =v
-    ++end;
-    return true;
-}
-bool Vector::pushFront(double v) {
-      if(capacity==getSize() && !reserve(capacity+64)) return false;
-      for(int i=getSize()-1; i>=0;i--)
-            start[i+1] = start[i];
-      start[0] = v;
-      end++;
-      return true;
-}
-bool Vector::insertAt(int where, double v){
-      if(capacity==getSize() && !reserve(capacity+64))
+      double x; 
+      double y; 
+      point():x(0),y(0){};
+      point(double a, double b):x(a), y(b){};
+      bool operator<(const point& other){
+            if(other.x*other.x + other.y*other.y > x*x + y*y) return true;
             return false;
-      if(where>getSize()) where = getSize();
-
-      for(int i = getSize()-1;i>=where; --i)
-            start[i+1] = start[i];
-      start[where]= v;
-      end++;
-      return true;     
-}
-bool Vector::deleteBack(){
-      if(getSize()==0) return false;
-      end --;
-      return true;
-}
-bool Vector::deleteFront(){
-      if(getSize()==0) return false;
-      for(int i=1;i<getSize();++i)
-            start[i-1] = start[i];
-      end --;
-      return true;
-}
-class BadIndex{
-public:
-      int index;
-      BadIndex(int i):index(i){};
-};
-double & Vector::elementAt(int i) const{
-    if(i<0 || i >= getSize()) throw BadIndex(i);
-    return start[i];
-}
-void Vector::dump() const {
-    cout<<"[";
-    for(int i=0; i<getSize();++i)
-        cout<<start[i]<<" ";
-    cout<< "]\n";
-}
-
-
-//lista
-class ListElement{
-public:
-      ListElement * next; 
-      int value;
-};
-class List{
-public:
-      ListElement * start; 
-      ListElement * end;
-
-      List();
-      ~List();
-      void pushFront(int);
-      void pushBack(int);
-      void insertAt(int, int);
-      void deleteFront();
-      void deleteBack();
-      void dump() const;
-};
-List::List():start(nullptr), end(nullptr){}
-
-void List::pushFront(int v){
-      ListElement * le = new ListElement();
-      le->value = v; 
-      le->next = start;
-      start = le;
-      if(end==nullptr)end=start;
-}     
-void List::pushBack(int v){
-      ListElement * le = new ListElement();
-      le->value = v;
-      le->next = nullptr;
-      if(end) end->next = le;
-      end = le;
-      if(start==nullptr) start = end;
-}
-void List::insertAt(int where, int v){
-      ListElement * le = new ListElement;ListElement * prev = le;
-      le->value = v;
-      le->next = start;
-      for(int i = 0; i<where; ++i){
-            prev = le->next;
-            le->next = le->next->next;
       }
-      prev->next = le;
-}
-void List::dump(){
-      cout<<"[ ";
-      for(ListElement * i = start; i!=0; i=i->next)
-            cout<<i->value<<" ";
-      cout << " ]\n";
-}
-void List::deleteBack(){
-      ListElement * newEnd=0;
-      for(int i )
-}
+      void print(){cout<<x<<"\t"<<y<<"\n";}
+};
 int main(){
-      /*
-    Vector vect;
-    for(int i=0;i<5;i++){
-        vect.pushFront(i);
-        vect.pushBack(i+5);
-    }
-    vect.insertAt(5,100);
-    vect.insertAt(0,100);
-    vect.insertAt(1000,100);
-    vect.dump();
-    for( int i=0;i<6;i++){
-        vect.deleteBack();
-        vect.deleteFront();
-        vect.dump();
-    }
-    vect.elementAt(0)=-1;
-    vect.dump();
-    */
-   //kontenery 2
-   //wielokrotne przedłużanie wektora o kilkadziesiąt el. jest nieefektywne
+      list<point> list;
+      for(int i=0; i<10; ++i) list.push_back(point(rand()%234, rand()%234));
+      for(auto &element : list) element.print();
+      list.sort();
+      cout<<"\n";
+      for(auto &element : list) element.print();
+
+      map<string,int>dict;
+      dict.insert(map<string, int>::value_type("siemano", 3));
+      dict["c"]==1;
+      dict["a"]==3;dict["d"]==41;
+      dict["a"]=123;
+      map<string, int>::const_iterator element;
+      for(element = dict.begin(); element!=dict.end(); element++) cout<<element->first<<"\t"<<element->second<<"\n";
 
       return 0;
 }
