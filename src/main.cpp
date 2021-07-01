e5e4d501146ba619123f1ad8385e125a42894a57
Boro2001 <mikolajborowicz7@gmail.com>
2021-07-01 22:37:24 +0200

    comp

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,24 @@
 #include <iostream>
-#include <list>
-#include <map>
-#include <string>
-
+#include <vector>
+#include <math.h>
 using namespace std;
-class point{
-public:
-      double x; 
-      double y; 
-      point():x(0),y(0){};
-      point(double a, double b):x(a), y(b){};
-      bool operator<(const point& other){
-            if(other.x*other.x + other.y*other.y > x*x + y*y) return true;
-            return false;
-      }
-      void print(){cout<<x<<"\t"<<y<<"\n";}
-};
+
+
 int main(){
-      list<point> list;
-      for(int i=0; i<10; ++i) list.push_back(point(rand()%234, rand()%234));
-      for(auto &element : list) element.print();
-      list.sort();
-      cout<<"\n";
-      for(auto &element : list) element.print();
 
-      map<string,int>dict;
-      dict.insert(map<string, int>::value_type("siemano", 3));
-      dict["c"]==1;
-      dict["a"]==3;dict["d"]==41;
-      dict["a"]=123;
-      map<string, int>::const_iterator element;
-      for(element = dict.begin(); element!=dict.end(); element++) cout<<element->first<<"\t"<<element->second<<"\n";
-
+      int n=424;
+      int *length = new int[n];
+      int *array = new int[n]; for(int i = 0; i < n; ++i) array[i] = rand()%20;
+      
+      for(int i = 0; i < n; ++i){
+            length[i] = 1;
+            for(int j = 0; j < i; ++j)
+                  if(array[j] < array[i])
+                        length[i] = max(length[i], length[j]+1);
+      }
+      int max = 0;
+      for(int i = 0; i < n; ++i) max = (length[i] > max) ? length[i] : max;
+      for(int i = 0; i < n; ++i)cout<< array[i] << "\t";
+      cout<<max; 
       return 0;
 }
